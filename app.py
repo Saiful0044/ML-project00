9a9262c4c8625a0905029631d6c6659bbd09a7de
SaifulIslam <saifulislam003244@gmail.com>
2024-06-02 18:56:05 +0600

    data transformation

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from srcc.mlproject.exception import CustomException
 import sys
 from srcc.mlproject.components.data_ingestion import DataIngestion
 from srcc.mlproject.components.data_ingestion import DataIngestionConfig
-
+from srcc.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 
 
 if __name__=="__main__":
@@ -12,7 +12,15 @@ if __name__=="__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
+        
+        data_transformation_config = DataTransformationConfig()
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transfomation(train_data_path,test_data_path)
+        
+        
+        
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
